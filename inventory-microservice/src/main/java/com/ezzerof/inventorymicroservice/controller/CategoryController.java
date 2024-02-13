e03ef471a7dc165cbe45fcc00f4949699ee3f56c
Ezzerof <christian.bitca@gmail.com>
2024-02-13 21:14:07 +0000

    initialized controllers for Category and Product

---
+++ inventory-microservice/src/main/java/com/ezzerof/inventorymicroservice/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.ezzerof.inventorymicroservice.controller;
+
+import com.ezzerof.inventorymicroservice.model.Category;
+import com.ezzerof.inventorymicroservice.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+    private CategoryService service;
+
+    @GetMapping("/{categoryName}")
+    public ResponseEntity<Category> getCategoryByName(@PathVariable String categoryName) {
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        return null;
+    }
+
+    @PutMapping("{/categoryName}")
+    public ResponseEntity<Category> updateCategory(@PathVariable String categoryName, @RequestBody Category category) {
+        return null;
+    }
+
+    @DeleteMapping("{/categoryName}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable String categoryName) {
+        return null;
+    }
+}
--- inventory-microservice/src/main/java/com/ezzerof/inventorymicroservice/service/ProductService.java
+++ inventory-microservice/src/main/java/com/ezzerof/inventorymicroservice/service/ProductService.java
@@ -5,7 +5,7 @@ import com.ezzerof.inventorymicroservice.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    Product getProductById(Long productName);
+    Product getProductById(Long productId);
     Product updateProduct(Long productId, Product updatedProduct);
     void deleteProduct(Long productId);
     List<Product> getAllProducts();
