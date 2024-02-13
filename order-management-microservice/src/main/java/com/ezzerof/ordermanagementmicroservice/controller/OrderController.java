1f8496f93d343d2fe2d657893cf1ebd56e3b3136
Ezzerof <christian.bitca@gmail.com>
2024-02-13 18:22:15 +0000

    created inventory microservice

--- order-management-microservice/src/main/java/com/ezzerof/ordermanagementmicroservice/controller/OrderController.java
+++ order-management-microservice/src/main/java/com/ezzerof/ordermanagementmicroservice/controller/OrderController.java
@@ -1,36 +1,68 @@
 package com.ezzerof.ordermanagementmicroservice.controller;
 
+import com.ezzerof.ordermanagementmicroservice.exception.InvalidOrderException;
 import com.ezzerof.ordermanagementmicroservice.model.Order;
+import com.ezzerof.ordermanagementmicroservice.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
+@AllArgsConstructor
 public class OrderController {
+    private OrderService service;
 
     @GetMapping
-    public List<Order> getAllOrders() {
-        return null;
+    public ResponseEntity<List<Order>> getAllOrders() {
+        List<Order> orderList = service.getAllOrders();
+        return new ResponseEntity<>(orderList, HttpStatus.OK);
     }
 
     @GetMapping("/{orderId}")
-    public Order getOrder(@PathVariable Long orderId) {
-        return null;
+    public ResponseEntity<Order> getOrder(@PathVariable Long orderId) {
+        try {
+            Order order = service.getOrderById(orderId);
+            return new ResponseEntity<>(order, HttpStatus.OK);
+        }
+        catch (InvalidOrderException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping
-    public Order addOrder(@RequestBody Order order) {
-        return null;
+    public ResponseEntity<Order> addOrder(@RequestBody Order order) {
+        try {
+            Order addedOrder = service.addOrder(order);
+            return new ResponseEntity<>(addedOrder, HttpStatus.CREATED);
+        }
+        catch (InvalidOrderException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping("/{orderId}")
-    public Order updateOrder(@RequestBody Order order, @PathVariable Long orderId) {
-        return null;
+    public ResponseEntity<Order> updateOrder(@RequestBody Order order, @PathVariable Long orderId) {
+        try {
+            Order updatedOrder = service.updateOrder(orderId, order);
+            return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+        }
+        catch (InvalidOrderException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("{orderId}")
-    public void deleteOrder(@PathVariable Long orderId) {
-
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
+        try {
+            service.deleteOrder(orderId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        catch (InvalidOrderException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
